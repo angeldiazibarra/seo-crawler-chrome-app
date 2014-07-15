bbe0425e4717d09cb838260825b168f9e6f703ab
Angel Diaz Ibarra <angel.diaz.ibarra@bbva.com>
2014-07-15 18:18:11 +0200

    core(angular): working AngularJS app

--- development/js/app.js
+++ development/js/app.js
@@ -3,16 +3,21 @@
   var app = angular.module('myCrawler', [ ]);
   console.log('app.js load');
 
-  app.controller('CrawlerController', function(){
+  app.controller('CrawlerController', function($scope){
     console.log('CrawlerController load');
     
     this.website = web;
+    
+    $scope.CheckUrl = function(){
+      console.log(web);
+    };
     
   });
   
   var web = {
       hostname: null,
-      protocol: 'http://'
+      protocol: 'http://',
+      validurl: false
   }
   
 })();
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,1 @@
-!function(){var app=angular.module("myCrawler",[]);console.log("app.js load"),app.controller("CrawlerController",function(){console.log("CrawlerController load"),this.website=web});var web={hostname:null,protocol:"http://"}}();
+!function(){var app=angular.module("myCrawler",[]);console.log("app.js load"),app.controller("CrawlerController",function($scope){console.log("CrawlerController load"),this.website=web,$scope.CheckUrl=function(){console.log(web)}});var web={hostname:null,protocol:"http://",validurl:!1}}();
