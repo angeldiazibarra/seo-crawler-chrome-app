857cc94e144ed69b051295a079e03e3b5b3cc066
Angel Diaz Ibarra <angel.diaz.ibarra@bbva.com>
2014-07-15 17:53:27 +0200

    core(angular): working AngularJS app

--- development/js/app.js
+++ development/js/app.js
@@ -1,2 +1,18 @@
-/* TESTING CUSTOM SCRIPT */
-console.log('app.js loaded');
+(function(){ 
+    
+  var app = angular.module('myCrawler', [ ]);
+  console.log('app.js load');
+
+  app.controller('CrawlerController', function(){
+    console.log('CrawlerController load');
+    
+    this.website = web;
+    
+  });
+  
+  var web = {
+      hostname: null,
+      protocol: 'http://'
+  }
+  
+})();
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,1 @@
-console.log("app.js loaded");
+!function(){var app=angular.module("myCrawler",[]);console.log("app.js load"),app.controller("CrawlerController",function(){console.log("CrawlerController load"),this.website=web});var web={hostname:null,protocol:"http://"}}();
