dec375ae41b6c089d35a289b1fd6983644a92c2f
Angel Diaz Ibarra <angel.diaz.ibarra@bbva.com>
2014-07-17 17:19:11 +0200

    feat(service): GET Web content & headers service

--- development/js/app.js
+++ development/js/app.js
@@ -1,16 +1,38 @@
 (function(){ 
     
   var app = angular.module('myCrawler', [ ]);
-  console.log('app.js load');
+  // console.log('app.js load');
+  
+  app.factory('getUrl', function($http) {
+    return {
+      getUrlData: function(url) {       
+        $http.get(url)
+        .success(function(data, status, headers, config) {
+          console.log('success');
+          console.log(data);
+          console.log(status);
+          console.log(headers);
+          console.log(config);
+        })
+        .error(function(data, status, headers, config) {
+          console.log('error');
+          console.log(data);
+          console.log(status);
+          console.log(headers);
+          console.log(config);
+        });
+      }
+    }
+  });
 
-  app.controller('CrawlerController', function($scope){
-    console.log('CrawlerController load');
+  app.controller('CrawlerController', function($scope, getUrl){
+    // console.log('CrawlerController load');
     
     this.website = web;
     
     $scope.CheckUrl = function(){
       var checkUrl = web.hostname;
-      console.log(checkUrl);
+      // console.log(checkUrl);
       
       if(checkUrl.match(/^(http:\/\/)/i)){
           web.protocol = 'http://';
@@ -24,17 +46,24 @@
       web.hostname = checkUrl;
       
       if(checkUrl.match(/^(https?:\/\/)?([a-z0-9]+\.*-*)+(\.[a-z]{2,6})$/i)){
-        console.log('valid');
+        // console.log('valid');
         web.invalidurl = false;
       }else{
-        console.log('invalid');
+        // console.log('invalid');
         web.invalidurl = true;
       }
     };
     
     $scope.SelectProtocol = function(protocol){
-      console.log(protocol);
+      // console.log(protocol);
       web.protocol = protocol;
+    };
+    
+    $scope.DoCrawl = function(){
+      // console.log(web);
+      var url = web.protocol + web.hostname;
+      var robotsUrl = web.protocol + web.hostname + '/robots.txt';
+      getUrl.getUrlData(url);
     };
     
   });
